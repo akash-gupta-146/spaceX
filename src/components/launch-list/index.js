16889b79fc7b82db95ec6842d5f721ce2fdd964e
Akash Gupta <akash8.gupta@paytm.com>
2020-08-09 14:38:22 +0530

    Added launch list component

---
+++ src/components/launch-list/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import * as style from './style.module.css';
+import Card from '../card';
+
+const API_HOST ='https://api.spacexdata.com/v3';
+
+export default class LaunchList extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state= {
+            list:[],
+            start:1,
+            count:10
+        }
+        
+    }
+
+     componentDidMount = async () => {
+        let res = await fetch(`${API_HOST}/launches?limit=${this.state.count}`);
+        let data = await res.json();
+        console.log(data)
+        this.setState({list:data})
+
+        // const handleIntersection = (entries, observer) => {
+        //     entries.forEach((entry) => {
+        //         if (entry.intersectionRatio > prevRatio) {
+        //             console.log('Last row of data')
+        //         } else {
+        //             console.log('Not in viewport')
+        //         }
+        //         prevRatio = entry.intersectionRatio;
+        //     });
+        // }
+
+        // let observer = new IntersectionObserver(handleIntersection);
+        // observer.observe(document.querySelector('#loadmore'));
+        // let prevRatio = 0;
+
+    }
+
+    render = () =>{
+        return<>
+            <div className={style.launchList} id='list'>
+                <div>here</div>
+                {
+                    this.state && this.state.list.length && 
+                    this.state.list.map( (launch,i) => {
+                        return <Card launch={launch} key={`card${i}`} />
+                    } )
+                }
+            </div>
+            <div id='loadmore'></div>
+        </>
+    }
+}
