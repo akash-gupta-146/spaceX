d4c024db1a59e0b4dce16fc3610f70a5b0fa6c9a
Akash Gupta <akash8.gupta@paytm.com>
2020-08-09 19:11:27 +0530

    Added App state and synched with localstorage

--- src/App.js
+++ src/App.js
@@ -4,16 +4,70 @@ import './App.css';
 import LaunchList from './components/launch-list';
 import Filters from './components/filters';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>SpaceX Launch Programs</h1>
-      <div className='container'>
-        <Filters />
-        <LaunchList  />
-      </div>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+        year: '',
+        launch: '',
+        land: ''
+    }
+  }
+
+  setYear(year){
+    this.setState({year});
+}
+componentDidUpdate(){
+  console.log('Component Updated');
+  this.updateLocalstorage()
+
+}
+
+changeYear = (year) => {
+  this.setState({year})
+}
+
+changeLaunchFilter = (launch) => {
+  this.setState({launch})
+
+}
+
+changeLandingFilter = (land) => {
+  this.setState({land})
+}
+
+updateLocalstorage = () =>{
+  if(window){
+    let temp = JSON.stringify(this.state);
+    console.log(temp)
+    window.localStorage.setItem('configuration',temp)
+}
+}
+
+  componentDidMount(){
+    if(window){
+        if(window.localStorage.configuration){
+            let temp = JSON.parse(window.localStorage.configuration)
+            this.setState({...temp})
+        }else{
+            let temp = JSON.stringify(this.state);
+            console.log(temp)
+            window.localStorage.setItem('configuration',temp)
+        }
+    }
+}
+
+render(){
+  return <div className="App">
+  <h1>SpaceX Launch Programs</h1>
+  <div className='container'>
+    <Filters config={this.state} setYear={this.changeYear} setLaunch={this.changeLaunchFilter} setLand={this.changeLandingFilter} />
+    <LaunchList  config={this.state} year={this.state.year}  />
+  </div>
+</div> 
+}
 }
 
 export default App;
--- src/components/filters/index.js
+++ src/components/filters/index.js
@@ -4,37 +4,10 @@ import  './filter.css';
 export default class Filters extends React.Component{
     constructor(props){
         super(props);
-
-        this.state = {
-            year: '',
-            launch: '',
-            land: ''
-        }
     }
-
-    componentDidMount(){
-        if(window){
-            if(window.localStorage.configuration){
-                let temp = JSON.parse(window.localStorage.configuration)
-                this.setState({...temp})
-            }else{
-                let temp = JSON.stringify(this.state);
-                console.log(temp)
-                window.localStorage.setItem('configuration',temp)
-            }
-        }
-    }
-
-    componentDidUpdate(){
-        console.log('Component Updated');
-
-    }
-
-    setYear(year){
-        this.setState({year});
-    }
-
+    
     render() {
+        let years =['2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020',]
         return <div className='filterSections'>
             <h3>Filters</h3>
             
@@ -42,21 +15,11 @@ export default class Filters extends React.Component{
             <section className='filter'>
                 <label>Launch Year</label>
                 <div className='options'>
-                    <div onClick={()=>this.setYear('2006')}><span>2006</span></div>
-                    <div onClick={()=>this.setYear('2007')}><span>2007</span></div>
-                    <div onClick={()=>this.setYear('2008')}><span>2008</span></div>
-                    <div onClick={()=>this.setYear('2009')}><span>2009</span></div>
-                    <div onClick={()=>this.setYear('2010')}><span>2010</span></div>
-                    <div onClick={()=>this.setYear('2011')}><span>2011</span></div>
-                    <div onClick={()=>this.setYear('2012')}><span>2012</span></div>
-                    <div onClick={()=>this.setYear('2013')}><span>2013</span></div>
-                    <div onClick={()=>this.setYear('2014')}><span>2014</span></div>
-                    <div onClick={()=>this.setYear('2015')}><span>2015</span></div>
-                    <div onClick={()=>this.setYear('2016')}><span>2016</span></div>
-                    <div onClick={()=>this.setYear('2017')}><span>2017</span></div>
-                    <div onClick={()=>this.setYear('2018')}><span>2018</span></div>
-                    <div onClick={()=>this.setYear('2019')}><span>2019</span></div>
-                    <div onClick={()=>this.setYear('2020')}><span>2020</span></div>
+                    {
+                        years.map ( (year,i) => <div onClick={()=>this.props.setYear(year)} key={`year${i}`}>
+                            <span className={ this.props.config.year  === year ? 'active' : ''}>{ year }</span>
+                        </div>)
+                    }
                 </div>
             </section>
 
@@ -64,8 +27,12 @@ export default class Filters extends React.Component{
             <section className='filter'>
                 <label>Successfull Launch</label>
                 <div className='options'>
-                    <div><span>True </span></div>
-                    <div><span>False</span></div>
+                    <div onClick={()=>this.props.setLaunch('true')}>
+                        <span className={ this.props.config.launch  == 'true' ? 'active' : ''} >True </span>
+                    </div>
+                    <div onClick={()=>this.props.setLaunch('false')}>
+                        <span className={ this.props.config.launch  == 'false' ? 'active' : ''} >False</span>
+                    </div>
                 </div>
             </section>
 
@@ -73,8 +40,12 @@ export default class Filters extends React.Component{
             <section className='filter'>
                 <label>Successfull Landing</label>
                 <div className='options'>
-                    <div><span>True </span></div>
-                    <div><span>False</span></div>
+                    <div onClick={()=>this.props.setLand('true')}>
+                        <span className={ this.props.config.land  == 'true' ? 'active' : ''} >True </span>
+                    </div>
+                    <div onClick={()=>this.props.setLand('false')}>
+                        <span className={ this.props.config.land  == 'false' ? 'active' : ''} >False</span>
+                    </div>
                 </div>
             </section>
         </div>
