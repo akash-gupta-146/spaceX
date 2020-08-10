ebe2c7c10fc697979a8c16ca1239dc54f2a359c9
Akash Gupta <akash8.gupta@paytm.com>
2020-08-10 12:28:55 +0530

    fixed Not found UI bug

--- src/components/result/index.js
+++ src/components/result/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './style.css';
 
 export default function Result({type}) {
 
-    let image = type === 'loading' ? 
+    let image = type == 'loading' ? 
     'https://static.dribbble.com/users/503653/screenshots/3143656/fluid-loader.gif' 
     : 'https://sterngoff.com/wp-content/uploads/No-Results.svg';
 
-    return <div style={{position:'absolute',top:'50%',left:'60%',transform:'translate(-50%,-50%)'}}>
+    return <div  className='result'>
     <img src={image} alt='image' />
-    <div style={{textAlign:'center'}}>
+    <div className='text'>
         { type === 'loading' ? 'Loading...' : 'No Related Results'}
     </div>
         </div>
